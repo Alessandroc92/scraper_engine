aa6b9c018d04413586c265ce7bd379825cffafaa
Alessandro Ciapetti <alessandrociapetti92@gmail.com>
2026-02-12 13:50:05 +0100

    Rename parser file and include function to extract JSON LD from HTML string

---
+++ parsers/parsing_functions.py
@@ -0,0 +1,16 @@
+"""A module containing parsing functs for different types of inputs"""
+import extruct
+
+
+def json_ld_parser(html_object: str) -> list:
+    """Parsing JSON Linked Data from HTML string object
+
+    Args:
+        html_object (str): An html string object.
+
+    Returns:
+        list: A list of all the elements in the JSON Linked data
+    """
+    jslde = extruct.jsonld.JsonLdExtractor()
+    data = jslde.extract(html)
+    return data
